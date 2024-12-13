80cd458469eada18355c4dd392a018db8780d86d
Ijaas-Fathi <ijaasfathima@gmail.com>
2024-12-13 23:05:40 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import Footer from './Components/Footer';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
 import About from './Pages/About';
-import Contact from './Pages/Contact';
-import TutorialDetails from './Pages/TutorialDetails';
-import Tutorials from './Pages/Tutorials';
-import Registration from './Pages/Registration';
-import CourseDetails from './Pages/CourseDetails';
+
 
 function App() {
   return (
@@ -21,10 +17,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/tutorials" element={<Tutorials />}/>
-          <Route path="/tutorials/:id" element={<TutorialDetails />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/courseDetails" element={<CourseDetails />} />
           <Route path="*" element={<div>404 Page Not Found</div>} />
         </Routes>
         <Footer />
